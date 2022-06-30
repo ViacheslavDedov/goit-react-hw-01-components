54accdb8a78229a34fcdbcf6754382c6dd3e8b23
ViacheslavDedov <dedoff.v@gmail.com>
2022-06-30 15:14:42 +0300

    add devtools

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -39,6 +39,5 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.number.isRequired
 }
 
