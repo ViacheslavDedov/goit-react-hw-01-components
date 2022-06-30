a2c6891c8d46b033694bb95ade08b4e2a7f1e31b
ViacheslavDedov <dedoff.v@gmail.com>
2022-06-30 15:08:07 +0300

    task 1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,44 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
 
+export default function Profile ({ username, tag, location, avatar, stats }) {
 
+  return (
+  <div className={css.profile}>
+    <div className={css.description}>
+      <img
+        src={avatar}
+        alt={username}
+        className={css.avatar}
+      />
+      <p className={css.name}>{username}</p>
+      <p className={css.tag}>@{tag}</p>
+      <p className={css.location}>{location}</p>
+    </div>
 
-<div class="profile">
-  <div class="description">
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-      class="avatar"
-    />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
-  </div>
+    <ul className={css.stats}>
+      <li>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{stats.followers}</span>
+      </li>
+      <li>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{stats.views}</span>
+      </li>
+      <li>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{stats.likes}</span>
+      </li>
+    </ul>
+    </div>
+  )
+}
 
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
-    </li>
-  </ul>
-</div>
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.number.isRequired
+}
+
